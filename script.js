470e0bd148779280dcede74cc3460357418c32e6
unknown <yangzhihong90@hotmail.com>
2015-10-17 15:57:17 -0500

    added MainController to module

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function MainController($scope,$http) {
-	var onUserComplete=function(response){
-		$scope.user=response.data;
-	}
+(function(){
+	var app=angular.module("githubViewer",[])
+	function MainController($scope,$http) {
+		var onUserComplete=function(response){
+			$scope.user=response.data;
+		}
 	
-	var onError=function(reason){
-		$scope.error="could not fetch them"
-	}
-	$http.get("https://api.github.com/users/robconery").then(onUserComplete,onError);
+		var onError=function(reason){
+			$scope.error="could not fetch them"
+		}
+		$http.get("https://api.github.com/users/robconery").then(onUserComplete,onError);
 	
 
-	 $scope.message="hello";
-    }
+		$scope.message="hello";
+    };
+	
+	app.controller("MainController",MainController);
+}());
