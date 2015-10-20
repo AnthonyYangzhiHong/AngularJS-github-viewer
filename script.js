80b902a5228521c45bb11e243133a7deb60e2907
unknown <yangzhihong90@hotmail.com>
2015-10-20 11:46:37 -0500

    ng-repeat directive

--- script.js
+++ script.js
@@ -3,15 +3,21 @@
 	function MainController($scope,$http) {
 		var onUserComplete=function(response){
 			$scope.user=response.data;
-		}
+			$http.get($scope.user.repos_url).then(onRepos,onError);
+		};
 	
+		var onRepos=function(response){
+			$scope.repos=response.data;
+		};
+		
 		var onError=function(reason){
-			$scope.error="could not fetch them"
-		}
+			$scope.error="could not fetch the data";
+		};
 		//$http.get("https://api.github.com/users/robconery").then(onUserComplete,onError);
 		$scope.search=function(username){
+			console.log("search user");
 			$http.get("https://api.github.com/users/"+username).then(onUserComplete,onError);
-		}
+		};
 
 		$scope.message="hello";
     };
