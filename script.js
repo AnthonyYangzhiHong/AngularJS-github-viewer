1d3533a3e23cb6dd9e7a50ee99d1fd1f6205914f
unknown <yangzhihong90@hotmail.com>
2015-10-20 10:49:35 -0500

    model binding for username fulfilled

--- script.js
+++ script.js
@@ -8,8 +8,10 @@
 		var onError=function(reason){
 			$scope.error="could not fetch them"
 		}
-		$http.get("https://api.github.com/users/robconery").then(onUserComplete,onError);
-	
+		//$http.get("https://api.github.com/users/robconery").then(onUserComplete,onError);
+		$scope.search=function(username){
+			$http.get("https://api.github.com/users/"+username).then(onUserComplete,onError);
+		}
 
 		$scope.message="hello";
     };
