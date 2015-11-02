ea3fad68dec113b2e86ee0cb37beda7c4759e971
unknown <yangzhihong90@hotmail.com>
2015-11-02 14:00:07 -0600

    countdown service cancel feature fulfilled

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function(){
 	var app=angular.module("githubViewer",[])
-	function MainController($scope,$http) {
+	function MainController($scope,$http,$interval) {
 		var onUserComplete=function(response){
 			$scope.user=response.data;
 			$http.get($scope.user.repos_url).then(onRepos,onError);
@@ -13,13 +13,31 @@
 		var onError=function(reason){
 			$scope.error="could not fetch the data";
 		};
+		
+		var decrementCountdown=function(){
+			$scope.countdown-=1;
+			if($scope.countdown<1){
+				$scope.search($scope.username);
+			};
+		};
+		var countdownInterval=null;
+		var startCountdown=function(){
+			countdownInterval=$interval(decrementCountdown,1000,$scope.countdown);
+		}
 		//$http.get("https://api.github.com/users/robconery").then(onUserComplete,onError);
 		$scope.search=function(username){
 			console.log("search user");
 			$http.get("https://api.github.com/users/"+username).then(onUserComplete,onError);
+			if(countdownInterval){
+				$interval.cancel(countdownInterval);
+			}
 		};
 
-		$scope.message="hello";
+		$scope.message="Github Viewer";
+		$scope.username="angular";
+		$scope.repoSortOrder="-stargazers_count";
+		$scope.countdown=5;
+		startCountdown();
     };
 	
 	app.controller("MainController",MainController);
